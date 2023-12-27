9aed92730bc46e9ca6538802eba31523018e873b
Ayah Shraim <ayahshraim558@gmail.com>
2023-12-27 04:00:50 +0200

    Create the Resturant Reservation Db Context

---
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantReservation.Db
+{
+    public class RestaurantReservationDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public RestaurantReservationDbContext(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
