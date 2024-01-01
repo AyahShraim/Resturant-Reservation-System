bbc164af5ce7683c1a6b6b44e09aaed70ea72b50
Ayah Shraim <ayahshraim558@gmail.com>
2024-01-01 20:14:28 +0200

    Create/Implement/test customer crud operation

---
+++ RestaurantReservationApp/Invokers_SampleData/RepositoriesInvokers/CutomerRepositoryTest.cs
@@ -0,0 +1,101 @@
+﻿using RestaurantReservation.Db.DataModels;
+using RestaurantReservation.Db.Repositories;
+using RestaurantReservationApp.Utilities;
+
+namespace RestaurantReservationApp.Tests.RepositoryInvokers_SampleData
+{
+    public class CustomerRepositoryTest
+    {
+        private readonly IRepositoryServices<Customer, string> _customerRepository;
+
+        public CustomerRepositoryTest(IRepositoryServices<Customer, string> customerRepository)
+        {
+            _customerRepository = customerRepository ?? throw new ArgumentNullException(nameof(customerRepository));
+        }
+
+        public async Task TestGetAllAsync()
+        {
+            Console.WriteLine("\n\nTesting Get All Customers...");
+            var customers = await _customerRepository.GetAllAsync();
+            var pageSize = 4;
+            PaginationConsoleTable<Customer>.Paginate(customers, pageSize);
+        }
+
+        public async Task TestGetByIdAsync()
+        {
+            Console.WriteLine("\n\nTesting Get Customer by Id...");
+
+            int customerToGetId = 1;
+            var customer = await _customerRepository.GetByIdAsync(customerToGetId);
+            if (customer != null)
+            {
+                var pageSize = 1;
+                PaginationConsoleTable<Customer>.Paginate(new List<Customer> { customer! }, pageSize);
+            }
+            else
+            {
+                Console.WriteLine($"\nNo such customer with id:{customerToGetId}");
+            }
+        }
+
+        public async Task TestAddAsync()
+        {
+            Console.WriteLine("\n\nTesting Add Customer...");
+            var customer = new Customer
+            {
+                FirstName = "sample",
+                LastName = "customer",
+                Email = "sample_customer@gmail.com",
+                PhoneNumber = "1234567890",
+            };
+            var result = await _customerRepository.AddAsync(customer);
+            if (result.IsSuccess)
+            {
+                Console.WriteLine($"\nCustomer with {result.Message} added successfully");
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+
+        public async Task TestUpdateAsync()
+        {
+            Console.WriteLine("\n\nTesting Update Customer...");
+
+            var updatedCustomer = new Customer
+            {
+                FirstName = "newFName",
+                LastName = "newLName",
+                PhoneNumber = "12345678909",
+            };
+
+            int customerToUpdateId = 1;
+            var result = await _customerRepository.UpdateAsync(customerToUpdateId, updatedCustomer);
+            if (result.IsSuccess)
+            {
+                Console.WriteLine($"\nCustomer with {result.Message} updated successfully");
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+
+        public async Task TestDeleteAsync()
+        {
+            Console.WriteLine("\n\nTesting Delete Customer...");
+
+            int customerToDeleteId = 7;
+            var deleteResult = await _customerRepository.DeleteAsync(customerToDeleteId);
+            if (deleteResult)
+            {
+                Console.WriteLine($"\nCustomer with id:{customerToDeleteId} has been deleted successfully");
+            }
+            else
+            {
+                Console.WriteLine($"\nNo customer with id:{customerToDeleteId} found!");
+            }
+        }
+    }
+}
--- RestaurantReservationApp/Program.cs
+++ RestaurantReservationApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestaurantReservation.Db;
+using RestaurantReservation.Db.Repositories;
+using RestaurantReservationApp.Tests.RepositoryInvokers_SampleData;
+
+using var dbContext = new RestaurantReservationDbContext();
+
+#region Customer repository tests
+
+    var customerRepository = new CustomerRepository(dbContext);
+
+    var customerRepositoryTest = new CustomerRepositoryTest(customerRepository);
+
+    await customerRepositoryTest.TestGetAllAsync();
+
+    await customerRepositoryTest.TestGetByIdAsync();
+
+    await customerRepositoryTest.TestAddAsync();
+
+    await customerRepositoryTest.TestUpdateAsync();
+
+    await customerRepositoryTest.TestDeleteAsync();
+
+#endregion
+
