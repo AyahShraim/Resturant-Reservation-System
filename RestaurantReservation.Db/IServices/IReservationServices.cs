a226dee5e70336e39fe60e8846eef00eac1063a6
Ayah Shraim <ayahshraim558@gmail.com>
2024-01-01 21:30:32 +0200

    create IReservation service for the reservation functionality

---
+++ RestaurantReservation.Db/IServices/IReservationServices.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantReservation.Db.DataModels;
+using RestaurantReservation.Db.StoredProcedureModels;
+
+namespace RestaurantReservation.Db.IServices
+{
+    public interface IReservationServices
+    {
+        Task<IEnumerable<Reservation>> GetReservationsByCustomerAsync(int customerId);
+
+        Task <IEnumerable<CustomerWithLargePartySizeReservation>> GetCustomersWithLargePartyReservations(int partySizeThreshold);
+    }
+}
