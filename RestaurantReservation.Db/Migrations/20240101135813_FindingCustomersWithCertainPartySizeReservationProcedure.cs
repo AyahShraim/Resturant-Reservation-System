54d65dc17416a2ec3d99abe292891341fd6d3979
Ayah Shraim <ayahshraim558@gmail.com>
2024-01-01 21:36:13 +0200

    add Design a stored procedure for Finding all customers who have made reservations with a party size greater than a certain value using migration

---
+++ RestaurantReservation.Db/Migrations/20240101135813_FindingCustomersWithCertainPartySizeReservationProcedure.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class FindingCustomersWithCertainPartySizeReservationProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE OR ALTER PROCEDURE sp_FindCustomersWithThresholdLargerPartySize
+                    @PartySizeThreshold INT
+                    AS
+                    BEGIN
+                        SELECT DISTINCT 
+						    CustomerId,
+						    Customers.FirstName,
+						    Customers.LastName,
+						    Customers.Email,
+						    Reservations.Id,
+						    Reservations.PartySize
+                        FROM
+						    Customers 
+                        INNER JOIN
+						    Reservations 
+					    ON Customers.Id = Reservations.CustomerId
+                        WHERE Reservations.PartySize > @PartySizeThreshold;
+                    END
+                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS sp_FindCustomersWithLargePartySize;");
+        }
+    }
+}
