89a06145776487bb7f0cf16386e93d2a31f18e74
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 01:08:21 +0200

    add customer model and customer profile

---
+++ RestaurantReservation.API/Models/Customers/CustomerCreationDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantReservation.API.Models.Customers
+{
+    public class CustomerCreationDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ RestaurantReservation.API/Profiles/CustomerProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.Customers;
+using RestaurantReservation.Db.Entities;
+
+namespace RestaurantReservation.API.Profiles
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile() 
+        {
+            CreateMap<Customer, CustomersWithoutReservationsDto>();
+            CreateMap<CustomerCreationDto, Customer>();
+            CreateMap<Customer, CustomerCreationDto>();
+        }
+    }
+}
