e1286898526c576298bb3a42a8abc8f71bc26bc2
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 01:21:08 +0200

    add pagination metadata model

---
+++ RestaurantReservation.Db/Pagination/PaginationMetaData.cs
@@ -0,0 +1,19 @@
+﻿
+namespace RestaurantReservation.Db.Pagination
+{
+    public class PaginationMetaData
+    {
+        public int PageNumber { get; init; }
+        public int PageSize { get; init; }
+        public int TotalPages { get; init; }
+        public int TotalRecords { get; init; }
+
+        public PaginationMetaData(int totalRecords, int pageSize, int pageNumber)
+        {
+            PageNumber = pageNumber; 
+            PageSize = pageSize;
+            TotalRecords = totalRecords;
+            TotalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+        }
+    }
+}
