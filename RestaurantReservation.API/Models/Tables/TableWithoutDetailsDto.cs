1e90e91b39d1e3af22e57482fe85cc56579dbcbd
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 01:22:06 +0200

    add table model

---
+++ RestaurantReservation.API/Models/Tables/TableWithoutDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantReservation.API.Models.Tables
+{
+    public class TableWithoutDetailsDto
+    {
+        public int Id { get; set; }
+        public int Capacity { get; set; }
+        public int RestaurantId { get; set; }
+    }
+}
