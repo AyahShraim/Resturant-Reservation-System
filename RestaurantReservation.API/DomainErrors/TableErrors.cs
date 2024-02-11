eb62f0dd4e67f5f43e0c1d7371b083d0592279a5
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 01:18:56 +0200

    add table model and table profile

---
+++ RestaurantReservation.API/DomainErrors/TableErrors.cs
@@ -0,0 +1,10 @@
+﻿using RestaurantReservation.API.Models.ErrorsHandling;
+
+namespace RestaurantReservation.API.DomainErrors
+{
+    public class TableErrors
+    {
+        public static Error NotFound(int id) => Error.NotFound(
+           "Table.NotFound", $"Table with id {id} is not found");
+    }
+}
---
+++ RestaurantReservation.API/Profiles/TableProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.Tables;
+using RestaurantReservation.Db.Entities;
+
+namespace RestaurantReservation.API.Profiles
+{
+    public class TableProfile : Profile
+    {
+        public TableProfile()
+        {
+            CreateMap<Table, TableWithoutDetailsDto>();
+            CreateMap<Table, TableDto>();
+            CreateMap<TableDto, Table>();
+        }
+    }
+}
