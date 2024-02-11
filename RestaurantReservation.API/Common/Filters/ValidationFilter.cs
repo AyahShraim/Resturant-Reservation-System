6271e09a74196ff5ca5f8437b9ae74f6e58576b1
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 01:05:35 +0200

    add filter for model validation

---
+++ RestaurantReservation.API/Common/Filters/ValidationFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using RestaurantReservation.API.Models.ErrorsHandling;
+
+namespace RestaurantReservation.API.Filters
+{
+    public class ValidationFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if(!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState
+                     .Where(m => m.Value.Errors.Any())
+                     .SelectMany(m => m.Value.Errors.Select(e => new ErrorModel
+                     {
+                         FieldName = m.Key,
+                         Message = e.ErrorMessage
+                     }))
+                     .ToList();
+
+                var errorResponse = new ErrorResponse
+                {
+                    Errors = errors
+                };
+
+                context.Result = new BadRequestObjectResult(errorResponse);
+                return;
+            }
+            await next();    
+        }
+    }
+}
