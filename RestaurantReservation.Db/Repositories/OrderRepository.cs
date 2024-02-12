17855c4aafdd0557156a1ead6631586780a70504
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 08:21:37 +0200

    update rest of db repositories

--- RestaurantReservation.Db/Repositories/OrderRepository.cs
+++ RestaurantReservation.Db/Repositories/OrderRepository.cs
@@ -1,89 +1,16 @@
-﻿using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using RestaurantReservation.Db.Entities;
 using RestaurantReservation.Db.Interfaces;
-using RestaurantReservation.Db.Utilities;
 
 namespace RestaurantReservation.Db.Repositories
 {
-    public class OrderRepository : IRepositoryServices<Order, string>, IOrderServices
+    public class OrderRepository : Repository<Order>, IOrderServices
     {
         private readonly RestaurantReservationDbContext _dbContext;
 
-        public OrderRepository(RestaurantReservationDbContext dbContext)
+        public OrderRepository(RestaurantReservationDbContext dbContext) : base(dbContext)
         {
             _dbContext = dbContext;
-        }
-
-        public async Task<OperationResult<string>> AddAsync(Order order)
-        {
-            try
-            {
-                await _dbContext.Orders.AddAsync(order);
-                await _dbContext.SaveChangesAsync();
-                return OperationResult<string>.SuccessResult($"id:{order.Id}");
-            }
-            catch (DbUpdateException ex) when (IsForeignKeyViolation(ex))
-            {
-                return OperationResult<string>.FailureResult("Foreign key constraint violation. Make sure associated entities exist.");
-            }
-            catch (Exception ex)
-            {
-                return OperationResult<string>.FailureResult($"An error occurred while processing the order.{ex.Message}");
-            }
-
-            bool IsForeignKeyViolation(DbUpdateException ex)
-            {
-                var sqlException = ex.InnerException as SqlException;
-
-                return sqlException?.Number == 547;
-            }
-        }
-
-        public async Task<bool> DeleteAsync(int id)
-        {
-            var orderToDelete = await _dbContext.Orders.FindAsync(id);
-            if (orderToDelete != null)
-            {
-                _dbContext.Orders.Remove(orderToDelete);
-                await _dbContext.SaveChangesAsync();
-                return true;
-            }
-            return false;
-        }
-
-        public async Task<OperationResult<string>> UpdateAsync(int id, Order updatedOrder)
-        {
-            try
-            {
-                var rowsEffected = await _dbContext.Orders
-                    .Where(order => order.Id == id)
-                    .ExecuteUpdateAsync(updates =>
-                        updates.SetProperty(order => order.Date, updatedOrder.Date)
-                               .SetProperty(order => order.TotalAmount, updatedOrder.TotalAmount)
-                               .SetProperty(order => order.EmployeeId, updatedOrder.EmployeeId)
-                               .SetProperty(order => order.ReservationId, updatedOrder.ReservationId));
-                if (rowsEffected == 0)
-                {
-                    return OperationResult<string>.FailureResult($"No order with id {id} found for the update.");
-                }
-
-                return OperationResult<string>.SuccessResult($"id: {id}");
-            }
-            catch (Exception ex)
-            {
-                return OperationResult<string>.FailureResult($"An unexpected error occurred. {ex.Message}");
-            }
-        }
-
-        public async Task<IEnumerable<Order>> GetAllAsync()
-        {
-            return await _dbContext.Orders.ToListAsync();
-        }
-
-        public async Task<Order?> GetByIdAsync(int id)
-        {
-            return await _dbContext.Orders.FindAsync(id);
         }
 
         public async Task<IEnumerable<Order>> ListOrdersAndMenuItemsAsync(int reservationId)
@@ -105,15 +32,6 @@ namespace RestaurantReservation.Db.Repositories
                 .Distinct()
                 .ToListAsync();
             return orderedMenuItems;
-        }
-
-        public async Task<decimal> CalculateAverageOrderAmountAsync(int employeeId)
-        {
-            var averageOrderAmount = await _dbContext.Orders
-                .Where(order => order.EmployeeId == employeeId)
-                .AverageAsync(order => (decimal?)order.TotalAmount);
-               
-            return averageOrderAmount.HasValue? averageOrderAmount.Value : 0 ;
         }
     }
 }
--- RestaurantReservation.Db/Repositories/ReservationRepository.cs
+++ RestaurantReservation.Db/Repositories/ReservationRepository.cs
@@ -3,96 +3,30 @@ using Microsoft.EntityFrameworkCore;
 using RestaurantReservation.Db.Entities;
 using RestaurantReservation.Db.Interfaces;
 using RestaurantReservation.Db.StoredProcedureModels;
-using RestaurantReservation.Db.Utilities;
 
 namespace RestaurantReservation.Db.Repositories
 {
-    public class ReservationRepository : IRepositoryServices<Reservation, string>, IReservationServices
+    public class ReservationRepository : Repository<Reservation>, IReservationServices
     {
         private readonly RestaurantReservationDbContext _dbContext;
 
-        public ReservationRepository(RestaurantReservationDbContext dbContext)
+        public ReservationRepository(RestaurantReservationDbContext dbContext) : base(dbContext)
         {
             _dbContext = dbContext;
         }
 
-        public async Task<OperationResult<string>> AddAsync(Reservation reservation)
+        public async Task<bool> MatchingTableRestaurant(int restaurantId, int tableId)
         {
-            try
-            {
-                var matchingTableRestaurant = await _dbContext.Tables
-                    .Where(
-                    table => table.Id == reservation.TableId && table.RestaurantId == reservation.RestaurantId)
-                    .FirstOrDefaultAsync();
+            var matchingTableRestaurant = await _dbContext.Tables
+                .Where(
+                table => table.Id == tableId && table.RestaurantId == restaurantId)
+                .FirstOrDefaultAsync();  
 
-                if (matchingTableRestaurant == null)
-                {
-                    return OperationResult<string>.FailureResult("Invalid Reservation. The associated table does not exist or does not belong to the specified restaurant");
-                }
-                await _dbContext.Reservations.AddAsync(reservation);
-                await _dbContext.SaveChangesAsync();
-                return OperationResult<string>.SuccessResult($"id: {reservation.Id}");
-            }
-            catch (DbUpdateException ex) when (IsForeignKeyViolation(ex))
+            if(matchingTableRestaurant == null)
             {
-                return OperationResult<string>.FailureResult("Foreign key constraint violation. Make sure associated entities exist.");
+                return false;
             }
-            catch (Exception ex)
-            {
-                return OperationResult<string>.FailureResult($"An error occurred while processing the reservation.{ex.Message}");
-            }
-            bool IsForeignKeyViolation(DbUpdateException ex)
-            {
-                var sqlException = ex.InnerException as SqlException;
-
-                return sqlException?.Number == 547;
-            }
-        }
-
-
-        public async Task<bool> DeleteAsync(int id)
-        {
-            var reservationToDelete = await _dbContext.Reservations.FindAsync(id);
-            if (reservationToDelete != null)
-            {
-                _dbContext.Reservations.Remove(reservationToDelete);
-                await _dbContext.SaveChangesAsync();
-                return true;
-            }
-            return false;
-        }
-
-        public async Task<OperationResult<string>> UpdateAsync(int id, Reservation updatedReservation)
-        {
-            try
-            {
-                var rowsEffected = await _dbContext.Reservations.Where(reservation => reservation.Id == id)
-                .ExecuteUpdateAsync(updates =>
-                     updates.SetProperty(reservation => reservation.Date, updatedReservation.Date)
-                            .SetProperty(reservation => reservation.PartySize, updatedReservation.PartySize)
-                            .SetProperty(reservation => reservation.CustomerId, updatedReservation.CustomerId)
-                            .SetProperty(reservation => reservation.TableId, updatedReservation.TableId)
-                            .SetProperty(reservation => reservation.RestaurantId, updatedReservation.RestaurantId));
-                if (rowsEffected == 0)
-                {
-                    return OperationResult<string>.FailureResult($"No reservation with id {id} found for the update.");
-                }
-                return OperationResult<string>.SuccessResult($"id: {id}");
-            }
-            catch (Exception ex)
-            {
-                return OperationResult<string>.FailureResult($"An error occurred while updating the reservation.{ex.Message}");
-            }
-        }
-
-        public async Task<IEnumerable<Reservation>> GetAllAsync()
-        {
-            return await _dbContext.Reservations.ToListAsync();
-        }
-
-        public async Task<Reservation?> GetByIdAsync(int id)
-        {
-            return await _dbContext.Reservations.FindAsync(id);
+            return true;
         }
 
         public async Task<IEnumerable<Reservation>> GetReservationsByCustomerAsync(int customerId)
