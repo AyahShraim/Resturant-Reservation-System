ff4db1bf06376368dfd2f0012caf2a43e27ad966
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 14:49:54 +0200

    generate jwt token

---
+++ RestaurantReservation.API/Authentication/IJwtTokenService.cs
@@ -0,0 +1,9 @@
+﻿using RestaurantReservation.API.Models.Authentication;
+
+namespace RestaurantReservation.API.Authentication
+{
+    public interface IJwtTokenService
+    {
+        Task<string> TryGenerateToken(AuthenticationCredentials authenticationCredentials);
+    }
+}
---
+++ RestaurantReservation.API/Authentication/SigningConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace RestaurantReservation.API.Authentication
+{
+    public class SigningConfiguration
+    {
+        public SymmetricSecurityKey SymmetricSecurityKey { get; }
+        public SigningCredentials SigningCredentials { get; }   
+        public SigningConfiguration(string key)
+        {
+            SymmetricSecurityKey =  new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(key));
+
+            SigningCredentials = new SigningCredentials(
+                SymmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+        }
+    }
+}
