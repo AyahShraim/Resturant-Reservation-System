69fa5708e724556785e2ebc31bdef310cd79f32a
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 08:25:45 +0200

    add order items model,profiles

---
+++ RestaurantReservation.API/Models/OrderItems/OrderItemWithoutDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantReservation.API.Models.OrderItems
+{
+    public class OrderItemWithoutDetailsDto
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int OrderId { get; set; }
+        public int MenuItemId { get; set; }
+    }
+}
---
+++ RestaurantReservation.API/Profiles/OrderItemProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.MenuItems;
+using RestaurantReservation.API.Models.OrderItems;
+using RestaurantReservation.Db.Entities;
+
+namespace RestaurantReservation.API.Profiles
+{
+    public class OrderItemProfile : Profile
+    {
+        public OrderItemProfile()
+        {
+            CreateMap<OrderItem, OrderItemWithoutDetailsDto>();
+            CreateMap<OrderItem, OrderItemDto>();
+            CreateMap<OrderItemDto, OrderItem>();
+        }
+    }
+}
