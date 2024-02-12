5e5feae7340910ac6c7b2b1d2304b80dabc659dc
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 08:26:48 +0200

    add reservation model,profiles

---
+++ RestaurantReservation.API/Models/Reservations/ReservationWithoutDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantReservation.API.Models.Reservations
+{
+    public class ReservationWithoutDetailsDto
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int PartySize { get; set; }
+        public int RestaurantId { get; set; }
+        public int CustomerId { get; set; }
+        public int TableId { get; set; }
+    }
+}
