47430a1220c687cf870b88737361feaa6a2e1e0d
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 14:50:28 +0200

    add authentication endpoint

---
+++ RestaurantReservation.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Authentication;
+using RestaurantReservation.API.Models.Authentication;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public readonly IJwtTokenService _jwtTokenService;
+        public AuthenticationController(IJwtTokenService jwtTokenService)
+        {
+            _jwtTokenService = jwtTokenService;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<ActionResult<string>> Authenticate(AuthenticationCredentials authenticationCredentials)
+        {
+            var token = await _jwtTokenService.TryGenerateToken(authenticationCredentials);
+            if(token == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(new
+            {
+                success = true,
+                message = "Token generated successfully.",
+                token
+            });
+        }
+    }
+}
