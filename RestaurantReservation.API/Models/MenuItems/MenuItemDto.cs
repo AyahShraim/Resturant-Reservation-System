33e4b790dc32479f7b8b620702501703beee9300
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 08:25:19 +0200

    add menu items model,profiles

---
+++ RestaurantReservation.API/Models/MenuItems/MenuItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantReservation.API.Models.MenuItems
+{
+    public class MenuItemDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string? Description { get; set; }
+  
+        public decimal Price { get; set; }
+        public int RestaurantId { get; set; }
+    }
+}
