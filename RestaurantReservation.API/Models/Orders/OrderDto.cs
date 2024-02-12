9bb0f385cbfafb9dc141b62b04443e95a2275cc3
Ayah Shraim <ayahshraim558@gmail.com>
2024-02-12 08:26:11 +0200

    add order model,profiles

---
+++ RestaurantReservation.API/Models/Orders/OrderDto.cs
@@ -0,0 +1,19 @@
+﻿using RestaurantReservation.Db.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantReservation.API.Models.Orders
+{
+    public class OrderDto
+    {
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public int ReservationId { get; set; }
+    }
+}
